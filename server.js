d641c94300fb391778ef221ef575faaddf24205f
Mikkel Ofrim <39702459+Mikkelof@users.noreply.github.com>
2022-02-21 09:54:36 +0100

    OIIIII

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 const content = document.getElementById("codeTextArea").value
 
-function writeToFile() {fs.writeFile('/resources/code.java', content, err => {
+function writeToFile() {fs.writeFile('./resources/code.java', content, err => {
   if (err) {
     console.error(err)
     return
